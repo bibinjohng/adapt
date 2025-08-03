36ceb3f8163f6590d5883a36db6a49dad9b90c28
bibinjohng <bibinjohng@gmail.com>
2025-08-04 00:41:05 +1000

    test

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -9,7 +9,7 @@
         />
       </NuxtLink>
       <ul class="flex gap-8 text-md font-semibold">
-        <li><NuxtLink to="/" class="hover:text-[#1181c3] active:text-[#1181c3] focus:text-[#1181c3]">Home</NuxtLink></li>
+        <li><NuxtLink to="/" class="hover:text-[#1181c3] active:text-[#1181c3] focus:text-[#1181c3]">Home testing</NuxtLink></li>
         <li><NuxtLink to="/about" class="hover:text-[#1181c3] active:text-[#1181c3] focus:text-[#1181c3]">About Us</NuxtLink></li>
         <li><NuxtLink to="/services" class="hover:text-[#1181c3] active:text-[#1181c3] focus:text-[#1181c3]">Our Services</NuxtLink></li>
         <li><NuxtLink to="/contact" class="hover:text-[#1181c3] active:text-[#1181c3] focus:text-[#1181c3]">Contact Us</NuxtLink></li>
